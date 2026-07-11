4d96f2e34696cc3efb05466fd8ba1e110fdb3ebc
ItsNotAI LABS <MedinaSITECH@outlook.com>
2026-07-11 00:11:53 -0500

    Export commercial platform primitives

--- capsula/__init__.py
+++ capsula/__init__.py
@@ -1,6 +1,30 @@
 """CAPSULA Studio runtime package."""
 
+from .commercial import ActivationChecklist, PricingPlan, list_plans, recommend_plan
 from .models import CapsuleManifest, CapsuleSession, RuntimeSpec
 from .orchestrator import CapsulaOrchestrator
+from .release_gate import ReleaseGate, default_release_gate
+from .templates import CapsuleTemplate, get_template, list_templates
+from .telemetry import TelemetrySink, activation_health
+from .workspace import Workspace, CapsuleProject, WorkspaceMember
 
-__all__ = ["CapsuleManifest", "CapsuleSession", "RuntimeSpec", "CapsulaOrchestrator"]
+__all__ = [
+    "CapsuleManifest",
+    "CapsuleSession",
+    "RuntimeSpec",
+    "CapsulaOrchestrator",
+    "ActivationChecklist",
+    "PricingPlan",
+    "list_plans",
+    "recommend_plan",
+    "ReleaseGate",
+    "default_release_gate",
+    "CapsuleTemplate",
+    "get_template",
+    "list_templates",
+    "TelemetrySink",
+    "activation_health",
+    "Workspace",
+    "CapsuleProject",
+    "WorkspaceMember",
+]
